af3e0644754a34315b5665b8ff43f2d825d37473
Dhanarajb <88299676+Dhanarajb@users.noreply.github.com>
2023-03-02 12:25:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cart-using-context-Reactjs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-d2bi9s)
---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './style.css';
+import Cart from './components/Cart';
+import Total from './components/Total';
+import TotalContext from './store/total-context';
+
+function App() {
+  const products = [
+    { pName: 'Apple', price: 20 },
+    { pName: 'Banana', price: 3 },
+    { pName: 'Orange', price: 10 },
+    { pName: 'Grapes', price: 8 },
+  ];
+  const [cart, setProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  let handleProdChange = (e) => {
+    let pName = e.target.options[e.target.selectedIndex].text;
+    let price = e.target.value;
+    let tCart = [...cart];
+    let obj = { pName, price };
+    let tPrice = parseInt(price);
+    tPrice = total + tPrice;
+    tCart.push(obj);
+
+    setProducts(tCart);
+    setTotal(tPrice);
+  };
+
+  return (
+    <div className="App">
+      <div className="customDiv">
+        <h3>Purchase Component</h3>
+        <hr />
+        <select onChange={handleProdChange}>
+          {products.map((product, index) => {
+            return (
+              <option value={product.price} key={index}>
+                {product.pName} - ${product.price}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
+      <TotalContext.Provider value={total}>
+        <Cart cart={cart} />
+        <Total />
+      </TotalContext.Provider>
+    </div>
+  );
+}
+
+export default App;
